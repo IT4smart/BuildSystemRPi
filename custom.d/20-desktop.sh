a92a7fc622d7dbf911876c7e003681feb91a1fc3
Raphael Lekies <raphael.lekies@it4s.eu>
2016-09-20 11:00:06 +0200

    enable audio add support for keyboard shortcuts (ConfigPage and Terminal)

--- custom.d/20-desktop.sh
+++ custom.d/20-desktop.sh
@@ -5,13 +5,20 @@
 # copy .xsession
 cp custom.d/files/Desktop/.xsession "$R/home/pi/.xsession"
 
-# install nodm and qt5-default IT4S Apps
-APT_INCLUDES="nodm qt5-default psmisc freerdp-x11"
+# copy .xbindkeysrc for keybindings
+cp custom.d/files/Desktop/.xbindkeysrc "$R/home/pi/.xbindkeysrc"
+
+# install nodm and qt5-default for IT4S Apps
+APT_INCLUDES="nodm qt5-default psmisc freerdp-x11 xbindkeys"
 chroot_exec apt-get -qq -y install ${APT_INCLUDES}
 
 # install StartPage
 cp custom.d/files/IT4S/armv7-startpage.deb "$R/tmp/armv7-startpage.deb"
 chroot_exec dpkg -i /tmp/armv7-startpage.deb
+
+# install ConfigPage
+cp custom.d/files/IT4S/armv7-configpage.deb "$R/tmp/armv7-configpage.deb"
+chroot_exec dpkg -i /tmp/armv7-configpage.deb
 
 # configure nodm
 cp custom.d/files/Desktop/nodm "$R/etc/default/nodm"
