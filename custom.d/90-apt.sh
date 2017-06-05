be919ae794be1f5c9471cf7c36151e5159a78f47
Raphael Lekies <raphael.lekies@it4s.eu>
2017-06-05 12:44:26 +0200

    bugfix after merge from 'raphael.lekies/BuildSystemRPi'

--- custom.d/90-apt.sh
+++ custom.d/90-apt.sh
@@ -3,16 +3,8 @@
 #
 
 # set default raspberry pi packages
-<<<<<<< HEAD
-#APT_INCLUDES="libraspberrypi0 flash-kernel"
-APT_INCLUDES="libraspberrypi0"
-
-# see all hold packages
-chroot_exec apt-mark showhold 
-=======
 #APT_INCLUDES="${APT_INCLUDES} libraspberrypi0 flash-kernel"
 APT_INCLUDES="${APT_INCLUDES} libraspberrypi0 lsb-release"
 
->>>>>>> 0cdb28c1c161e1be791abd18d10effd4bd336443
 
 chroot_exec apt-get -qq -y install ${APT_INCLUDES}
