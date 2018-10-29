78a460be63df7b3a607b26740d1e8fb94572e5d2
hack3d <hack3d.dev@googlemail.com>
2018-10-29 11:13:38 +0100

    Changed company name. Also changed some directories.

--- custom.d/20-desktop.sh
+++ custom.d/20-desktop.sh
@@ -13,18 +13,20 @@ APT_INCLUDES="nodm qt5-default psmisc freerdp-x11 xbindkeys"
 chroot_exec apt-get -qq -y install ${APT_INCLUDES}
 
 # install StartPage
-cp custom.d/files/IT4S/startpage_0.9.5_armhf.deb "$R/tmp/armv7-startpage.deb"
-chroot_exec dpkg -i /tmp/armv7-startpage.deb
+#cp custom.d/files/IT4S/startpage_0.9.5_armhf.deb "$R/tmp/armv7-startpage.deb"
+#chroot_exec dpkg -i /tmp/armv7-startpage.deb
 
 # install ConfigPage
-cp custom.d/files/IT4S/configpage_0.9.3_armhf.deb "$R/tmp/armv7-configpage.deb"
-chroot_exec dpkg -i /tmp/armv7-configpage.deb
+#cp custom.d/files/IT4S/configpage_0.9.3_armhf.deb "$R/tmp/armv7-configpage.deb"
+#chroot_exec dpkg -i /tmp/armv7-configpage.deb
+
+chroot_exec apt-get install -qq -y startpage configpage
 
 # copy cleanup script
-cp custom.d/files/IT4S/cleanup.sh "$R/opt/IT4S/cleanup.sh"
+cp custom.d/files/IT4smart/cleanup.sh "$R/opt/IT4smart/cleanup.sh"
 
 # copy it4smart logo
-cp custom.d/files/IT4S/IT4smart_Logo_small.png "$R/opt/IT4S/startpage/Ressources/ass-logo.png"
+cp custom.d/files/IT4smart/IT4smart_Logo_small.png "$R/opt/IT4smart/startpage/Ressources/ass-logo.png"
 
 # configure nodm
 cp custom.d/files/Desktop/nodm "$R/etc/default/nodm"
--- custom.d/files/Firstboot/26-change_permissions.sh
+++ custom.d/files/Firstboot/26-change_permissions.sh
@@ -4,4 +4,4 @@ logger -t "rc.firstboot" "change permission"
 sudo chown -R pi:pi /home/pi/.ICAClient
 
 # Change permission for IT4S
-sudo chown -R pi:pi /opt/IT4S
+sudo chown -R pi:pi /opt/IT4smart
